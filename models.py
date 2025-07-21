f403ebf0b85f1a79d08b82dcac8d596c09cb0562
sridhar281994 <sri.christh@gmail.com>
2025-07-22 03:43:15 +0530

    Update models.py
    Update models.py

--- models.py
+++ models.py
@@ -4,13 +4,15 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)  # email or username
     password_hash = Column(String, nullable=False)
     upi_id = Column(String, nullable=False)
     wallet_cents = Column(BigInteger, default=0)
     is_active_in_stage = Column(Boolean, default=False)
     active_stage = Column(Integer, nullable=True)
     active_join_ts = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    otp = Column(String, nullable=True)  # stores OTP value
+    otp_expiry = Column(TIMESTAMP(timezone=True), nullable=True)  # stores OTP expiry time
 class Transaction(Base):
     __tablename__ = "transactions"
     id = Column(Integer, primary_key=True)
